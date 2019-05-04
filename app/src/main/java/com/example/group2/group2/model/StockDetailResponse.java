33e9fb0e255bd7d2b9c5b5d9a3f3058b4de0acd6
praveen guduru <PRAVEEN.GUDURU1@T-Mobile.com>
2019-05-04 14:22:57 +0530

    details response file

--- app/src/main/java/com/example/group2/group2/model/StockDetailResponse.java
+++ app/src/main/java/com/example/group2/group2/model/StockDetailResponse.java
@@ -10,7 +10,89 @@ public class StockDetailResponse implements Parcelable{
     @SerializedName("01. symbol")
     @Expose
     public String symbol;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getOpen() {
+        return open;
+    }
+
+    public void setOpen(String open) {
+        this.open = open;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getLatestTradingDay() {
+        return latestTradingDay;
+    }
+
+    public void setLatestTradingDay(String latestTradingDay) {
+        this.latestTradingDay = latestTradingDay;
+    }
+
+    public String getPreviousClose() {
+        return previousClose;
+    }
+
+    public void setPreviousClose(String previousClose) {
+        this.previousClose = previousClose;
+    }
+
+    public String getChange() {
+        return change;
+    }
+
+    public void setChange(String change) {
+        this.change = change;
+    }
+
+    public String getChangePercent() {
+        return changePercent;
+    }
+
+    public void setChangePercent(String changePercent) {
+        this.changePercent = changePercent;
+    }
+
     @SerializedName("02. open")
+
     @Expose
     public String open;
     @SerializedName("03. high")
